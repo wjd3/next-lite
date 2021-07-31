b43f977efd0403158054131133704e16ab303b9f
Liam Davis <liamdavis@tuta.io>
2021-07-31 07:27:23 -0600

    feat(tailwind.config.js): configure tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import 'src/styles/global.css';
+import Head from 'next/head';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>next-lite</title>
+      </Head>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 };
 
